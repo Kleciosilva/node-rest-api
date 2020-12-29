5e573c578f79226c547d4c7d2c49f3f67459a32b
Klécio Silva <kleciosilvajp@gmail.com>
2020-12-28 22:36:22 -0300

    Save data

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,8 +1,12 @@
+const Atendimento = require('../models/Atendimentos')
+
 module.exports = app => {
   app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos S2'))
 
   app.post('/atendimentos', (req, res) => {
-    console.log(req.body)
-    res.send('Você está na rota de atendimentos [POST]')
+    const atendimento = req.body
+
+    Atendimento.adiciona(atendimento)
+    res.send('POST atendimento')
   })
 }
